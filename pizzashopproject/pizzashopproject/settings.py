cfde175c10958951a471abb0b55a8e9e756305f8
egor3101 <52127280+egor3101@users.noreply.github.com>
2021-02-01 19:53:09 +0300

    update_1

--- pizzashopproject/pizzashopproject/settings.py
+++ pizzashopproject/pizzashopproject/settings.py
@@ -12,6 +12,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -22,6 +23,7 @@ SECRET_KEY = '_1p4-lh65sghs24+1-1a0@d_t@42oixw9qq=fvp(8e*c1dgjkt'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 # Application definition
 
@@ -67,6 +69,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pizzashopproject.wsgi.application'
 
+
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -76,6 +79,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -95,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -107,6 +112,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
